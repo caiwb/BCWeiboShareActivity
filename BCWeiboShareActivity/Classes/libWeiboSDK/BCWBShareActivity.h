6c100c7bf77954e0fc843b4f518b4987283f5ff3
caiwb <whenbber@gmail.com>
2016-07-21 04:36:46 +0800

    add example

---
+++ BCWeiboShareActivity/Classes/libWeiboSDK/BCWBShareActivity.h
@@ -0,0 +1,13 @@
+//
+//  BCWBShareActivity.h
+//  Pods
+//
+//  Created by caiwb on 16/7/21.
+//
+//
+
+#import "BCWBActivity.h"
+
+#import "BCWBLoginProvider.h"
+#import "BCWBSocialHandler.h"
+#import "BCWBShareProvider.h"
--- Example/BCWeiboShareActivity/BCViewController.m
+++ Example/BCWeiboShareActivity/BCViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "BCViewController.h"
+#import "BCWBShareActivity.h"
 
 @interface BCViewController ()
 
@@ -14,16 +15,54 @@
 
 @implementation BCViewController
 
+- (void)loginAndGetUserInfo
+{
+    [BCWBLoginProvider loginWithCompleteBlock:^(BOOL suc, NSString *accessToken, NSString *openId, NSString *errMsg) {
+        if (suc)
+        {
+            [BCWBLoginProvider getUserInfoWithCompleteBlock:^(BOOL suc, WeiboUser *userInfo) {
+                if (suc)
+                {
+                    //use userInfo
+                }
+            }];
+        }
+    }];
+}
+
+- (void)share
+{
+    [BCWBShareProvider shareWebPage:@"分享到微博"
+                          withTitle:@"标题"
+                               text:@"内容"
+                         thumbImage:[UIImage imageNamed:@"thumbImage"]
+                           complete:^(BOOL suc, NSString *errMsg) {
+                             //to do after share
+                         }];
+}
+
+- (void)shareOnActivityController
+{
+    BCWBActivity *item = [[BCWBActivity alloc] init];
+    
+    item.title = @"title";
+    item.text = @"content";
+    item.thumbImage = [UIImage new];
+    item.webUrl = @"github.com";
+    [item setCompleteBlock:^(BOOL suc, NSString *errMsg) {
+        //to do after share
+    }];
+    
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:@[item.title, item.webUrl, item.thumbImage] applicationActivities:@[item]];
+    [self presentViewController:activityVC animated:YES completion:nil];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    [self loginAndGetUserInfo];
 }
 
 @end
